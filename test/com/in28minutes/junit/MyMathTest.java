59d37f895a6e2228701d678b2ce956bcb025360e
Tong <jeremy.tong@capgemini.com>
2018-09-20 16:29:24 -0400

    this feels good

--- test/com/in28minutes/junit/MyMathTest.java
+++ test/com/in28minutes/junit/MyMathTest.java
@@ -1,11 +1,16 @@
 package com.in28minutes.junit;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
 
 class MyMathTest {
 
 	@Test
-	void test() {
+	void test_sum_with3numbers() {
+		MyMath myMath = new MyMath();
+		int result = myMath.sum(new int[] { 1, 2, 3 });
+		assertEquals(6, result);
 	}
 
 }
