4029a51c147efad21d57e676d929ccfd026f20e2
Tong <jeremy.tong@capgemini.com>
2018-09-21 08:59:19 -0400

    @Before runs before every test

--- test/com/in28minutes/junit/MyMathTest.java
+++ test/com/in28minutes/junit/MyMathTest.java
@@ -2,15 +2,30 @@ package com.in28minutes.junit;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import org.junit.Before;
 import org.junit.jupiter.api.Test;
 
 class MyMathTest {
 
+	@Before
+	public void before() {
+		System.out.println("Before");
+	}
+
 	@Test
-	void test_sum_with3numbers() {
+	public void test_sum_with3numbers() {
+		System.out.println("Test1");
 		MyMath myMath = new MyMath();
 		int result = myMath.sum(new int[] { 1, 2, 3 });
 		assertEquals(6, result);
+	}
+
+	@Test
+	public void test_sum_with1number() {
+		System.out.println("Test2");
+		MyMath myMath = new MyMath();
+		int result = myMath.sum(new int[] { 3 });
+		assertEquals(3, result);
 	}
 
 }
